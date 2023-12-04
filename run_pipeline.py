ab6bb38a376163460caf5d6edc7e05038b94c1b9
UTSAVMON <utsavmondal16@gmail.com>
2023-12-04 21:13:13 +0530

    Transfer

---
+++ run_pipeline.py
@@ -0,0 +1,4 @@
+from pipelines.training_pipeline import training_pipeline
+
+if __name__ == "__main__":
+    training_pipeline(data_path="C:/Users/utsav/Desktop/MLops/data/olist_customers_dataset.csv")
